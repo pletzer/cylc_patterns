c38dc57e0ad2642f830786110157e9abdc131423
Alex Pletzer <alexander@gokliya.net>
2023-09-08 11:33:07 +1200

    work in progress

--- cylc-src/find_best/update.py
+++ cylc-src/find_best/update.py
@@ -2,10 +2,13 @@ import defopt
 import sympy
 import random
 import numpy
+import glob
+import re
+from matplotlib import pylab
 
 
-#3OBJFUN_EXPR = '1 + 2*(x-3)**4'
-OBJFUN_EXPR = '1 - 0.2*(x-3)**2*cos((x-3)*2*pi)'
+#OBJFUN_EXPR = '1 + 2*(x-3)**4'
+OBJFUN_EXPR = '1 + 0.1*(x-3)**2 - 0.2 * cos((x-3)*2*pi)'
 
 def update(x0, objfun_expr) -> float:
     x = sympy.Symbol('x')
@@ -51,6 +54,47 @@ def merge(paramfilea: str, paramfileb: str, *, outfile: str) -> None:
     numpy.save(outfile, xnew)
 
 
+def plot(*, directory: str='./', outfile: str='update.png') -> None:
+    """
+    Plot the search
+    :param directory: directory where the params output files reside
+    :param outfile: output png file
+    """
+    xvals = {}
+    yvals = {}
+    xmin = float('inf')
+    xmax = - float('inf')
+
+    x = sympy.Symbol('x')
+    f = sympy.parsing.sympy_parser.parse_expr(OBJFUN_EXPR)
+
+    for filename in glob.glob(directory + '/params*.npy'):
+        # extract the level and instance
+        m = re.search(r'params(\d+)\_(\d+)', filename)
+        level, i = int(m.group(1)), int(m.group(2))
+        # read the param 
+        xi = float(numpy.load(filename, allow_pickle=True))
+        fi = f.evalf(subs={x: xi})
+        xvals[level] = xvals.get(level, []) + [xi]
+        yvals[level] = yvals.get(level, []) + [fi]
+        if xi < xmin:
+            xmin = xi
+        if xi > xmax:
+            xmax = xi
+
+    xvals2 = numpy.linspace(xmin, xmax, 100)
+    fvals2 = sympy.utilities.lambdify(x, OBJFUN_EXPR, 'numpy')(xvals2)
+    pylab.plot(xvals2, fvals2, 'b-')
+
+    nlevels = max(xvals.keys()) + 1
+    for level in range(nlevels):
+        xnorm = level/float(nlevels)
+        clr = (1 - xnorm, xnorm, 0.) #numpy.sin(xnorm*numpy.pi)**2)
+        pylab.plot(xvals[level], yvals[level], color=clr, marker='+', linestyle='None')
+
+    pylab.savefig(outfile)
+
+
 
 if __name__ == '__main__':
-    defopt.run([init, merge])
+    defopt.run([init, merge, plot])
